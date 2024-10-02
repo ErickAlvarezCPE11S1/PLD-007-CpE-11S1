2fa938652a1be9001489d5f807d7da7683cbd3c8
ErickAlvarezCPE11S1 <qejalvarez02@tip.edu.ph>
2024-10-02 12:43:51 +0800

    Add files via upload

---
+++ Practical Lab.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int sum;
+int number[5];
+int count;
+
+while(count < 5){
+	
+cout << "Enter number: ";
+cin >> count[number];
+count++;
+}
+sum = number[0]+number[4];
+cout << "Your sum is: "<< sum;
+return 0;
+}
